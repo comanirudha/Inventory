b56fd05768e9524a6c0a178c0a7ce0d3e420ec24
Andre Azzolini <apazzolini@gmail.com>
2013-01-09 22:03:59 -0600

    Replace tabs with spaces

--- src/main/java/org/broadleafcommerce/inventory/checkout/service/workflow/InventoryCompensatingCheckoutErrorHandler.java
+++ src/main/java/org/broadleafcommerce/inventory/checkout/service/workflow/InventoryCompensatingCheckoutErrorHandler.java
@@ -44,57 +44,57 @@ import java.util.Map;
 @Component("blInventoryCompensatingCheckoutErrorHandler")
 public class InventoryCompensatingCheckoutErrorHandler implements ErrorHandler {
 
-	private static final Log LOG = LogFactory.getLog(InventoryCompensatingCheckoutErrorHandler.class);
-	
-	@Resource(name="blInventoryService")
-	protected InventoryService inventoryService;
-	
+    private static final Log LOG = LogFactory.getLog(InventoryCompensatingCheckoutErrorHandler.class);
+    
+    @Resource(name="blInventoryService")
+    protected InventoryService inventoryService;
+    
     @SuppressWarnings("unused")
     private String name;
     
     private final int maxRetries = 5;
-	
-	@Override
-	public void setBeanName(String name) {
-		this.name = name;
-	}
+    
+    @Override
+    public void setBeanName(String name) {
+        this.name = name;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void handleError(ProcessContext context, Throwable th)
-			throws WorkflowException {
-		context.stopProcess();
-		
-		CheckoutSeed seed = ((CheckoutContext) context).getSeedData();
-		
-		// The DecrementInventoryActivity, if successful, adds the inventory that was decremented to the context.  If we 
-		// find that, then we need to attempt to compensate for that inventory.
-		if (seed.getUserDefinedFields() != null && seed.getUserDefinedFields().get("BLC_INVENTORY_DECREMENTED") != null) {
-			Map<Sku, Integer> inventoryToIncrement = (Map<Sku, Integer>)seed.getUserDefinedFields().get("BLC_INVENTORY_DECREMENTED");
-			if (! inventoryToIncrement.isEmpty()) {
-				int retryCount = 0;
-	
-		        while (retryCount < maxRetries) {
-		            try {
-		            	inventoryService.incrementInventory(inventoryToIncrement);
-		            	break;
-		            } catch (ConcurrentInventoryModificationException ex) {
-		                retryCount++;
-		                if (retryCount == maxRetries) {
-		                    LOG.error("After an exception was encountered during checkout, where inventory was decremented. " + maxRetries + " attempts were made to compensate, " +
-		                    		"but were unsuccessful for order ID: " + seed.getOrder().getId() + ". This should be corrected manually!", ex);
-		                }
-		            } catch (RuntimeException ex) {
-		            	LOG.error("An unexpected error occured in the error handler of the checkout workflow trying to compensate for inventory. This happend for order ID: " +
-		            			seed.getOrder().getId() + ". This should be corrected manually!", ex);
-		            	break;
-		            }
-		        }
-			}
-		}
-		
+    @SuppressWarnings("unchecked")
+    @Override
+    public void handleError(ProcessContext context, Throwable th)
+            throws WorkflowException {
+        context.stopProcess();
+        
+        CheckoutSeed seed = ((CheckoutContext) context).getSeedData();
+        
+        // The DecrementInventoryActivity, if successful, adds the inventory that was decremented to the context.  If we 
+        // find that, then we need to attempt to compensate for that inventory.
+        if (seed.getUserDefinedFields() != null && seed.getUserDefinedFields().get("BLC_INVENTORY_DECREMENTED") != null) {
+            Map<Sku, Integer> inventoryToIncrement = (Map<Sku, Integer>)seed.getUserDefinedFields().get("BLC_INVENTORY_DECREMENTED");
+            if (! inventoryToIncrement.isEmpty()) {
+                int retryCount = 0;
+    
+                while (retryCount < maxRetries) {
+                    try {
+                        inventoryService.incrementInventory(inventoryToIncrement);
+                        break;
+                    } catch (ConcurrentInventoryModificationException ex) {
+                        retryCount++;
+                        if (retryCount == maxRetries) {
+                            LOG.error("After an exception was encountered during checkout, where inventory was decremented. " + maxRetries + " attempts were made to compensate, " +
+                                    "but were unsuccessful for order ID: " + seed.getOrder().getId() + ". This should be corrected manually!", ex);
+                        }
+                    } catch (RuntimeException ex) {
+                        LOG.error("An unexpected error occured in the error handler of the checkout workflow trying to compensate for inventory. This happend for order ID: " +
+                                seed.getOrder().getId() + ". This should be corrected manually!", ex);
+                        break;
+                    }
+                }
+            }
+        }
+        
         LOG.error("An error occurred during the workflow", th);
         throw new WorkflowException(th);
-	}
+    }
 
 }
